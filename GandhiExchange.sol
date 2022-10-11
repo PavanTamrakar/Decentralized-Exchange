95458a438eddd65fd022876795ca5194ba574ce5
Pavan Tamrakar <72463719+PavanTamrakar@users.noreply.github.com>
2022-10-11 19:41:36 +0530

    Update GandhiExchange.sol

--- GandhiExchange.sol
+++ GandhiExchange.sol
@@ -5,33 +5,33 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Exchange is ERC20 {
 
-    address public cryptoDevTokenAddress;
+    address public GandhiMoneyTokenAddress;
 
-    constructor(address _CryptoDevtoken) ERC20("CryptoDev LP Token", "CDLP") {
-        require(_CryptoDevtoken != address(0), "Token address passed is a null address");
-        cryptoDevTokenAddress = _CryptoDevtoken;
+    constructor(address _GandhiMoneytoken) ERC20("GandhiMoney LP Token", "CDLP") {
+        require(_GandhiMoneytoken != address(0), "Token address passed is a null address");
+        gandhiMoneyTokenAddress = _GandhiMoneytoken;
     }
     
     function getReserve() public view returns (uint) {
-        return ERC20(cryptoDevTokenAddress).balanceOf(address(this));
+        return ERC20(gandhiMoneyTokenAddress).balanceOf(address(this));
    }
    
    function addLiquidity(uint _amount) public payable returns (uint) {
        uint liquidity;
        uint ethBalance = address(this).balance;
-       uint cryptoDevTokenReserve = getReserve();
-       ERC20 cryptoDevToken = ERC20(cryptoDevTokenAddress);
+       uint gandhiMoneyTokenReserve = getReserve();
+       ERC20 gandhiMoneyToken = ERC20(gandhiMoneyTokenAddress);
    
-       if(cryptoDevTokenReserve == 0) {
-           cryptoDevToken.transferFrom(msg.sender, address(this), _amount);
+       if(gandhiMoneyTokenReserve == 0) {
+           gandhiMoneyToken.transferFrom(msg.sender, address(this), _amount);
            liquidity = ethBalance;
            _mint(msg.sender, liquidity);
    } else {
         uint ethReserve =  ethBalance - msg.value;
-        uint cryptoDevTokenAmount = (msg.value * cryptoDevTokenReserve)/(ethReserve);
-        require(_amount >= cryptoDevTokenAmount, "Amount of tokens sent is less than the minimum tokens required");
+        uint gandhiMoneyTokenAmount = (msg.value * gandhiMoneyTokenReserve)/(ethReserve);
+        require(_amount >= gandhiMoneyTokenAmount, "Amount of tokens sent is less than the minimum tokens required");
 
-        cryptoDevToken.transferFrom(msg.sender, address(this), cryptoDevTokenAmount);
+        gandhiMoneyToken.transferFrom(msg.sender, address(this), gandhiMoneyTokenAmount);
         liquidity = (totalSupply() * msg.value)/ ethReserve;
         _mint(msg.sender, liquidity);
     }
@@ -43,14 +43,14 @@ contract Exchange is ERC20 {
         uint _totalSupply = totalSupply();
         uint ethAmount = (ethReserve * _amount)/ _totalSupply;
   
-        uint cryptoDevTokenAmount = (getReserve() * _amount)/ _totalSupply;
+        uint gandhiMoneyTokenAmount = (getReserve() * _amount)/ _totalSupply;
  
         _burn(msg.sender, _amount);
     
         payable(msg.sender).transfer(ethAmount);
     
-        ERC20(cryptoDevTokenAddress).transfer(msg.sender, cryptoDevTokenAmount);
-        return (ethAmount, cryptoDevTokenAmount);
+        ERC20(gandhiMoneyTokenAddress).transfer(msg.sender, gandhiMoneyTokenAmount);
+        return (ethAmount, gandhiMoneyTokenAmount);
     }
          
       
